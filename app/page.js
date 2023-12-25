67dc69e5429de4e8e95f6a55589ae84cce4a1d57
Yash <78804783+yashd-dev@users.noreply.github.com>
2023-12-26 01:29:55 +0530

    Completed For Now

--- app/page.js
+++ app/page.js
@@ -3,199 +3,227 @@ import React, { useState } from "react";
 import Head from "next/head";
 
 const FileUploadForm = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    domain: "",
-    description: "",
-    techDomains: [],
-    reel: false,
-    posts: false,
-  });
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		domain: "",
+		description: "",
+		techDomains: [],
+		reel: false,
+		posts: false,
+	});
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setFormData({ ...formData, [name]: value });
+	};
 
-  const handleDomainSelection = (event) => {
-    const { name, checked } = event.target;
-    if (name === "reel" || name === "posts") {
-      setFormData({ ...formData, [name]: checked });
-    } else {
-      let updatedTechDomains = [...formData.techDomains];
+	const handleDomainSelection = (event) => {
+		const { name, checked } = event.target;
+		if (name === "reel" || name === "posts") {
+			setFormData({ ...formData, [name]: checked });
+		} else {
+			let updatedTechDomains = [...formData.techDomains];
 
-      if (checked) {
-        updatedTechDomains = [...updatedTechDomains, name];
-      } else {
-        updatedTechDomains = updatedTechDomains.filter(
-          (domain) => domain !== name
-        );
-      }
+			if (checked) {
+				updatedTechDomains = [...updatedTechDomains, name];
+			} else {
+				updatedTechDomains = updatedTechDomains.filter(
+					(domain) => domain !== name
+				);
+			}
 
-      setFormData({ ...formData, techDomains: updatedTechDomains });
-    }
-  };
+			setFormData({ ...formData, techDomains: updatedTechDomains });
+		}
+	};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
 
-    try {
-      const response = await fetch(process.env.NEXT_PUBLIC_APPSCRIPT_URL, {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+		try {
+			const response = await fetch(
+				process.env.NEXT_PUBLIC_APPSCRIPT_URL,
+				{
+					method: "POST",
+					mode: "no-cors",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(formData),
+				}
+			);
 
-      console.log(response)
+			console.log(response);
 
-      if (response) {
-        console.log("Form submitted successfully!");
-        alert("Form submitted successfully!");
-      } else {
-        throw new Error("Failed to submit form");
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  };
+			if (response) {
+				console.log("Form submitted successfully!");
+				alert("Form submitted successfully!");
+			} else {
+				throw new Error("Failed to submit form");
+			}
+		} catch (error) {
+			console.error("Error submitting form:", error);
+		}
+	};
 
-  const techDomains = [
-    "ML",
-    "Cyber Security",
-    "Web Dev",
-    "DSA",
-    "Cloud Computing",
-    "Data Science",
-  ];
-
-  const handleDomainForm = () => {
-    if (formData.domain === "technical") {
-      return (
-        <div>
-          <label>Technical Domains:</label>
-          {techDomains.map((domain, index) => (
-            <div key={index}>
-              <input
-                type="checkbox"
-                id={domain}
-                name={domain}
-                value={domain}
-                onChange={handleDomainSelection}
-              />
-              <label htmlFor={domain}>{domain}</label>
-            </div>
-          ))}
-        </div>
-      );
-    } else if (formData.domain === "creatives") {
-      return (
-        <div>
-          <label>Creative Domains:</label>
-          <div>
-            <input
-              type="checkbox"
-              id="reel"
-              name="reel"
-              onChange={handleDomainSelection}
-            />
-            <label htmlFor="reel">Reel</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              id="posts"
-              name="posts"
-              onChange={handleDomainSelection}
-            />
-            <label htmlFor="posts">Posts</label>
-          </div>
-        </div>
-      );
-    }
-    return null;
-  };
-
-  return (
-    <div className="max-w-lg mx-auto  mt-8 p-6 bg-gray-200 text-gray-900 rounded-lg">
-      <Head>
-        <title>NaiTechh Registrations</title>
-      </Head>
-      <h1 className="text-4xl font-bold  text-center m-4 ">
-        NaiTechh Registrations
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="name" className="block mb-1">
-            Name:
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            className="border rounded px-2 py-1 w-full"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block mb-1">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            className="border rounded px-2 py-1 w-full"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="domain" className="block mb-1">
-            Select Domain:
-          </label>
-          <select
-            id="domain"
-            name="domain"
-            value={formData.domain}
-            onChange={handleInputChange}
-            className="border rounded px-2 py-1 w-full"
-            required
-          >
-            <option value="">Choose Domain</option>
-            <option value="technical">Technical</option>
-            <option value="creatives">Creatives</option>
-          </select>
-        </div>
-        {handleDomainForm()}
-        <div className="mb-4">
-          <label htmlFor="description" className="block mb-1">
-            How can you help in growing naitechh:
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            value={formData.description}
-            onChange={handleInputChange}
-            className="border rounded px-2 py-1 w-full"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+	const techDomains = [
+		"ML",
+		"Cyber Security",
+		"Web Dev",
+		"DSA",
+		"Cloud Computing",
+		"Data Science",
+	];
+	const handleDomainForm = () => {
+		if (formData.domain === "technical") {
+			return (
+				<div>
+					<label className=" font-semibold py-2 mb-2">
+						Technical Domains:
+					</label>
+					<div className=" pt-2 grid grid-cols-2">
+						{techDomains.map((domain, index) => (
+							<div key={index} className="px-4 py-2 flex gap-3">
+								<input
+									type="checkbox"
+									id={domain}
+									name={domain}
+									value={domain}
+									onChange={handleDomainSelection}
+									className="bg-slate-100 border-purple-300 text-pink-500 focus:ring-0 focus:outline-none rounded shadow"
+								/>
+								<label htmlFor={domain}>{domain}</label>
+							</div>
+						))}
+					</div>
+				</div>
+			);
+		} else if (formData.domain === "creatives") {
+			return (
+				<div>
+					<label>Creative Domains:</label>
+					<div className="px-4 py-2 flex gap-3">
+						<input
+							type="checkbox"
+							id="reel"
+							name="reel"
+							onChange={handleDomainSelection}
+							className=" bg-slate-100 border-purple-300 text-red-500 focus:ring-0 focus:outline-none rounded shadow"
+						/>
+						<label htmlFor="reel">Reel</label>
+					</div>
+					<div className=" px-4 py-2 flex gap-3">
+						<input
+							type="checkbox"
+							id="posts"
+							name="posts"
+							onChange={handleDomainSelection}
+							className=" bg-slate-100 border-purple-300 text-red-500 focus:ring-0 focus:outline-none rounded shadow"
+						/>
+						<label htmlFor="posts">Posts</label>
+					</div>
+				</div>
+			);
+		}
+		return null;
+	};
+	return (
+		<div className=" min-h-screen max-h-screen w-full flex md:flex-row justify-center items-center lg:bg-[#181818] md:bg-[url('/grainny.png')] bg-no-repeat bg-cover overflow-hidden ">
+			<div className=" rounded-[0.5rem] text-white space-y-6 p-10  pb-16 md:min-w-[50vw] md:max-w-[50vw] h-full min-h-screen flex justify-center items-center md:items-start flex-col w-full grain overflow-y-scroll scrollbar">
+				<h1 className="text-3xl font-black text-center ">
+					Naitechh Registrations
+				</h1>
+				<form
+					onSubmit={handleSubmit}
+					className=" w-full max-w-md text-sm"
+				>
+					<div className="mb-4">
+						<label
+							htmlFor="name"
+							className="block mb-1 font-semibold"
+						>
+							Name:
+						</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							value={formData.name}
+							onChange={handleInputChange}
+							className="border-b border-gray-500  rounded px-2 py-2 w-full bg-white bg-opacity-5  backdrop-filter backdrop-blur-lg"
+							required
+						/>
+					</div>
+					<div className="mb-4">
+						<label
+							htmlFor="email"
+							className="block mb-1 font-semibold"
+						>
+							Email:
+						</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							value={formData.email}
+							onChange={handleInputChange}
+							className="border-b border-gray-500  rounded px-2 py-2 w-full bg-white bg-opacity-5  backdrop-filter backdrop-blur-lg"
+							required
+						/>
+					</div>
+					<div className="mb-4">
+						<label
+							htmlFor="domain"
+							className="block mb-1 font-semibold"
+						>
+							Select Domain:
+						</label>
+						<select
+							id="domain"
+							name="domain"
+							value={formData.domain}
+							onChange={handleInputChange}
+							className="border-b font-semibold border-gray-500 rounded px-2 py-2 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg md:bg-[#222] "
+							required
+						>
+							<option value="">Choose Domain</option>
+							<option value="technical"> Technical</option>
+							<option value="creatives" className="rounded-full">
+								Creatives
+							</option>
+						</select>
+					</div>
+					{handleDomainForm()}
+					<div className="my-4">
+						<label
+							htmlFor="description"
+							className="block mb-1 font-semibold text-base "
+						>
+							How can you contribute to the growth of Naitechh?"
+						</label>
+						<textarea
+							id="description"
+							name="description"
+							value={formData.description}
+							onChange={handleInputChange}
+							className="border-b border-gray-500  rounded px-2 py-5 w-full bg-white bg-opacity-5  backdrop-filter backdrop-blur-lg text-md"
+							required
+						/>
+					</div>
+					<button
+						type="submit"
+						className="bg-blue-500 text-white px-4 py-2 rounded"
+					>
+						Submit
+					</button>
+				</form>
+			</div>
+			<img
+				src="/bg.png"
+				className="hidden lg:block w-full min-w-[50vw] max-w-[50vw] h-full min-h-screen max-h-screen"
+			></img>
+		</div>
+	);
 };
 
 export default FileUploadForm;
