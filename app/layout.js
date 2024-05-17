a03090f48c12a3d49d20525b3da8d26b17704f6e
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-05-17 18:56:04 +0530

    Give up on SEO

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,19 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Head from "next/head";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'NeuroTechh Registrations',
-  description: 'If you wish to contribute to the coding community Neurotechh join here',
-}
+	title: "NeuroTechh Registrations",
+	description:
+		"We are a community of student developers who build projects and comeup with innovative solutions! Come join us and be the change!",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+		
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-
+import Head from "next/head";
 const FileUploadForm = () => {
 	const [loading, setLoading] = useState(false);
 	const [formData, setFormData] = useState({
@@ -148,106 +148,126 @@ const FileUploadForm = () => {
 	};
 
 	return (
-		<div className="min-h-screen max-h-screen w-full flex md:flex-row justify-center items-center lg:bg-[#181818] md:bg-[url('/grainny.png')] bg-no-repeat bg-cover overflow-hidden">
-			<div className="rounded-[0.5rem] text-white space-y-6 p-10 pb-16 md:min-w-[50vw] md:max-w-[50vw] h-full min-h-screen flex justify-center items-center md:items-start flex-col w-full grain overflow-y-scroll scrollbar md:overflow-hidden">
-				<h1 className="text-2xl tracking-wider font-black text-center">
-					Neurotechh Registrations
-				</h1>
-				<form
-					onSubmit={handleSubmit}
-					className="w-full max-w-md text-sm"
-				>
-					<div className="mb-4">
-						<label
-							htmlFor="name"
-							className="block mb-1 font-semibold"
-						>
-							Name:
-						</label>
-						<input
-							type="text"
-							id="name"
-							name="name"
-							value={formData.name}
-							onChange={handleInputChange}
-							className="border-b border-gray-500 rounded px-2 py-2 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg"
-							required
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							htmlFor="email"
-							className="block mb-1 font-semibold"
-						>
-							Email:
-						</label>
-						<input
-							type="email"
-							id="email"
-							name="email"
-							value={formData.email}
-							onChange={handleInputChange}
-							className="border-b border-gray-500 rounded px-2 py-2 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg"
-							required
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							htmlFor="domain"
-							className="block mb-1 font-semibold"
-						>
-							Select Domain:
-						</label>
-						<select
-							id="domain"
-							name="domain"
-							value={formData.domain}
-							onChange={handleInputChange}
-							className="border-b font-semibold border-gray-500 rounded px-2 py-2 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg md:bg-[#222] text-sm"
-							required
-						>
-							<option value="">Choose Domain</option>
-							<option value="technical"> Technical</option>
-							<option value="creatives" className="rounded-full">
-								Creatives
-							</option>
-						</select>
-					</div>
-					{handleDomainForm()}
-					<div className="my-4">
-						<label
-							htmlFor="description"
-							className="block mb-1 font-semibold "
-						>
-							How can you contribute to the growth of NeuroTechh?
-						</label>
-						<textarea
-							id="description"
-							name="description"
-							value={formData.description}
-							onChange={handleInputChange}
-							className="border-b border-gray-500 rounded px-2 py-5 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg text-base"
-							required
-						/>
-					</div>
-					<button
-						type="submit"
-						className="bg-blue-500 text-white px-4 py-2 rounded"
-						disabled={loading}
+		<>
+			<Head>
+				<meta
+					property="og:image"
+					content="https://neurotechh.live/seo.png"
+				/>
+				<meta property="og:image:type" content="<generated>" />
+				<meta property="og:image:width" content="<generated>" />
+				<meta property="og:image:height" content="<generated>" />
+
+				<meta name="twitter:image" content="<generated>" />
+				<meta name="twitter:image:type" content="<generated>" />
+				<meta name="twitter:image:width" content="<generated>" />
+				<meta name="twitter:image:height" content="<generated>" />
+			</Head>
+			<div className="min-h-screen max-h-screen w-full flex md:flex-row justify-center items-center lg:bg-[#181818] md:bg-[url('/grainny.png')] bg-no-repeat bg-cover overflow-hidden">
+				<div className="rounded-[0.5rem] text-white space-y-6 p-10 pb-16 md:min-w-[50vw] md:max-w-[50vw] h-full min-h-screen flex justify-center items-center md:items-start flex-col w-full grain overflow-y-scroll scrollbar md:overflow-hidden">
+					<h1 className="text-2xl tracking-wider font-black text-center">
+						Neurotechh Registrations
+					</h1>
+					<form
+						onSubmit={handleSubmit}
+						className="w-full max-w-md text-sm"
 					>
-						{loading ? "Submitting..." : "Submit"}
-					</button>
-				</form>
+						<div className="mb-4">
+							<label
+								htmlFor="name"
+								className="block mb-1 font-semibold"
+							>
+								Name:
+							</label>
+							<input
+								type="text"
+								id="name"
+								name="name"
+								value={formData.name}
+								onChange={handleInputChange}
+								className="border-b border-gray-500 rounded px-2 py-2 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg"
+								required
+							/>
+						</div>
+						<div className="mb-4">
+							<label
+								htmlFor="email"
+								className="block mb-1 font-semibold"
+							>
+								Email:
+							</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								value={formData.email}
+								onChange={handleInputChange}
+								className="border-b border-gray-500 rounded px-2 py-2 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg"
+								required
+							/>
+						</div>
+						<div className="mb-4">
+							<label
+								htmlFor="domain"
+								className="block mb-1 font-semibold"
+							>
+								Select Domain:
+							</label>
+							<select
+								id="domain"
+								name="domain"
+								value={formData.domain}
+								onChange={handleInputChange}
+								className="border-b font-semibold border-gray-500 rounded px-2 py-2 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg md:bg-[#222] text-sm"
+								required
+							>
+								<option value="">Choose Domain</option>
+								<option value="technical"> Technical</option>
+								<option
+									value="creatives"
+									className="rounded-full"
+								>
+									Creatives
+								</option>
+							</select>
+						</div>
+						{handleDomainForm()}
+						<div className="my-4">
+							<label
+								htmlFor="description"
+								className="block mb-1 font-semibold "
+							>
+								How can you contribute to the growth of
+								NeuroTechh?
+							</label>
+							<textarea
+								id="description"
+								name="description"
+								value={formData.description}
+								onChange={handleInputChange}
+								className="border-b border-gray-500 rounded px-2 py-5 w-full bg-white bg-opacity-5 backdrop-filter backdrop-blur-lg text-base"
+								required
+							/>
+						</div>
+						<button
+							type="submit"
+							className="bg-blue-500 text-white px-4 py-2 rounded"
+							disabled={loading}
+						>
+							{loading ? "Submitting..." : "Submit"}
+						</button>
+					</form>
+				</div>
+				<Image
+					src="/bg.png"
+					height={1000}
+					width={1000}
+					priority={true}
+					className="hidden lg:block w-full min-w-[50vw] max-w-[50vw] h-full min-h-screen max-h-screen"
+					alt="Background"
+				></Image>
 			</div>
-			<Image
-				src="/bg.png"
-				height={1000}
-				width={1000}
-				priority={true}
-				className="hidden lg:block w-full min-w-[50vw] max-w-[50vw] h-full min-h-screen max-h-screen"
-				alt="Background"
-			></Image>
-		</div>
+		</>
 	);
 };
 
