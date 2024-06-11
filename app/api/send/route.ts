7216685f9f46177399ee37ae976d74b99764b96f
Naitik Lodha <naitik.lodha139@nmims.edu.in>
2024-06-11 22:54:37 +0530

    feat:added bcc emails

---
+++ app/api/send/route.ts
@@ -0,0 +1,25 @@
+import { Resend } from 'resend';
+import { EmailTemplate } from '../../components/EmailTemplate';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request:Request) {
+  try {
+        const { name, email } = await request.json();
+    const { data, error } = await resend.emails.send({
+      from: 'registration@neurotechh.live',
+      to: [email],
+      bcc:["naitiklodha@neurotechh.live","yashdeshpande@neurotechh.live"],
+      subject: `Hello ${name}`,
+      react: EmailTemplate({ firstName: name }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
