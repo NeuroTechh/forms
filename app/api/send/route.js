962fd95048e3ec24d39bce3af92e9166c5b94c84
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-06-11 17:47:20 +0530

    praying to lords at vercel

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,24 +1,24 @@
-import { Resend } from 'resend';
-import { EmailTemplate } from '../../components/EmailTemplate';
+import { Resend } from "resend";
+import { EmailTemplate } from "../../components/EmailTemplate";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
-  try {
-        const { name, email } = await request.json();
-    const { data, error } = await resend.emails.send({
-      from: 'registration@neurotechh.live',
-      to: [email],
-      subject: `Hello ${name}`,
-      react: EmailTemplate({ firstName: name }),
-    });
+	try {
+		const { name, email } = await request.json();
+		const { data, error } = await resend.emails.send({
+			from: "registration@neurotechh.live",
+			to: [email],
+			subject: `Hello ${name}`,
+			react: EmailTemplate({ firstName: name }),
+		});
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+		if (error) {
+			return Response.json({ error }, { status: 500 });
+		}
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
+		return Response.json(data);
+	} catch (error) {
+		return Response.json({ error }, { status: 500 });
+	}
 }
